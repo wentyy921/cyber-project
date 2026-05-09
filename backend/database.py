e092184c420dc1412c7b072c8496fafcbd437bbd
Developer <developer@example.com>
2026-05-09 17:50:36 +0300

    Use dotenv for dynamic database credentials

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,10 @@
 from sqlmodel import SQLModel, create_engine
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/cyber_trainer"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost:5432/cyber_trainer")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
